f4e59d6b595fad74dde06c21cdadebd739c0ddde
1004404 <59642294+1004404@users.noreply.github.com>
2020-03-04 09:47:21 +0100

    Update pong-hoofdbestand.js

--- pong-hoofdbestand.js
+++ pong-hoofdbestand.js
@@ -1,10 +1,36 @@
+var x = 50;
+var y = 50;
+
 function setup()  {
-  var myCanvas = createCanvas(1000,1000);
-  background('black');
-  myCanvas.parent('processing');
-  framerate(50);
+  var myCanvas = createCanvas(800,800);
+  frameRate(50);
 }
 
 function draw() {
+  background('black');
+  if (keyIsDown(UP_ARROW)) {
+      y -= 3;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+     y += 3; 
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+     x -= 2;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+     x += 2;
+  }
   
+  x = constrain(x, 0, 200);
+  y = constrain(y, 0, 720);
+  rect(x,y,20,80);
+  tekenLijn(220,250,5,10,'red');
+  tekenLijn(220,275,5,10,'red');
+}
+
+function tekenLijn(x,y,w,h,kleur) {
+    push();
+    fill(kleur);
+    rect(x,y,w,h);
+    pop();
 }
