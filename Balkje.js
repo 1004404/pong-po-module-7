5ebcc14da032dfbac2dfe9bac68bbd4b40c867bc
1004404 <1004404@csgbogerman.nl>
2020-04-06 10:25:36 +0000

    shoutout naar johannes niffo hutst hutstshtusts wanneer nieuwe stream

---
+++ Balkje.js
@@ -0,0 +1,45 @@
+class Balkje {
+constructor(zijde) {
+    this.zijde = zijde;
+    if (this.zijde == "links") {
+        this.x = 50;
+    }
+
+    if (this.zijde == "rechts"){
+        this.x = 750;
+    }
+    this.y = 50;
+
+}
+
+bewegen(richting) {
+    if (richting == "omhoog") {
+        this.y -= 3;
+    }
+    else if (richting == "omlaag") {
+        this.y += 3; 
+    }
+    else if (richting == "links") {
+        this.x -= 2;
+    }
+    else if (richting == "rechts") {
+        this.x += 2;
+    }
+    if (this.zijde == "links"){
+        this.x = constrain(this.x, 0, 200);
+        this.y = constrain(this.y, 0, 720);
+    } 
+    if (this.zijde == "rechts"){
+        this.x = constrain(this.x,600,780);
+        this.y = constrain(this.y,0,720);
+    }
+
+
+}
+
+
+teken(){
+    rect(this.x,this.y,20,80); 
+}
+
+}
--- pong-hoofdbestand.js
+++ pong-hoofdbestand.js
@@ -1,18 +1,25 @@
-var x = 50;
-var y = 50;
+var linkerRichting = "geen";
+var rechterRichting = "geen";
 
 function setup()  {
   var myCanvas = createCanvas(800,800);
   frameRate(50);
+  Linkerbalkje = new Balkje("links");
+  Rechterbalkje = new Balkje("rechts");
 }
 
 function draw() {
-  background('black');
-
-
- 
-  tekenLijn(220,250,5,10,'red');
-  tekenLijn(220,275,5,10,'red');
+    background('black');
+    krijgRichting();
+    Linkerbalkje.bewegen(linkerRichting);
+    Rechterbalkje.bewegen(rechterRichting);
+    Linkerbalkje.teken();
+    Rechterbalkje.teken();
+    tekenLijn(220,250,5,10,'red');
+    tekenLijn(220,275,5,10,'red');
+    tekenLijn(220,300,5,10,'red');
+    tekenLijn(220,325,5,10,'red');
+    tekenLijn(220,350,5,10,'red');
 }
 
 function tekenLijn(x,y,w,h,kleur) {
@@ -20,4 +27,39 @@ function tekenLijn(x,y,w,h,kleur) {
     fill(kleur);
     rect(x,y,w,h);
     pop();
+}
+
+function krijgRichting() {
+if (keyIsDown(UP_ARROW)) {
+     rechterRichting = "omhoog";
+}
+else if (keyIsDown(DOWN_ARROW)) {
+     rechterRichting = "omlaag"; 
+}
+else if (keyIsDown(LEFT_ARROW)) {
+     rechterRichting = "links";
+  }
+else if (keyIsDown(RIGHT_ARROW)) {
+     rechterRichting = "rechts";
+}
+else {
+    rechterRichting = "geen";
+}
+
+
+if (keyIsDown(87)) {
+     linkerRichting = "omhoog";
+}
+else if (keyIsDown(83)) {
+     linkerRichting = "omlaag"; 
+}
+else if (keyIsDown(65)) {
+     linkerRichting = "links";
+  }
+else if (keyIsDown(68)) {
+     linkerRichting = "rechts";
+}
+else {
+    linkerRichting = "geen";
+}
 }
