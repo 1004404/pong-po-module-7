dd78a2996ec935134dadee377db7282655dab7ab
Jorrinho <1004071@csgbogerman.nl>
2020-04-10 11:49:35 +0000

    fea

--- pong-hoofdbestand.js
+++ pong-hoofdbestand.js
@@ -1,7 +1,7 @@
 var linkerRichting = "geen";
 var rechterRichting = "geen";
-let leftscore = 0;
-let rightscore = 0;
+var leftscore = 0;
+var rightscore = 0;
 
 function setup()  {
   var myCanvas = createCanvas(800,800);
